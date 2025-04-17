a3aacedcb3b14f1face68d9b76e50702a0cb07ee
vinodGaddagunta <150431844+vinodGaddagunt@users.noreply.github.com>
2025-04-17 20:20:08 +0530

    Update route.ts

--- src/app/api/transactions/[id]/route.ts
+++ src/app/api/transactions/[id]/route.ts
@@ -2,14 +2,14 @@ import { connectDB } from "@/lib/db";
 import { Transaction } from "@/models/transaction";
 import { NextResponse } from "next/server";
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(req: Request, { params }: { params: { id: number } }) {
   await connectDB();
   const data = await req.json();
   const updated = await Transaction.findByIdAndUpdate(params.id, data, { new: true });
   return NextResponse.json(updated);
 }
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(req: Request, { params }: { params: { id: number } }) {
   await connectDB();
   await Transaction.findByIdAndDelete(params.id);
   return NextResponse.json({ success: true });
